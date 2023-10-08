afd874746bc56cb0e7a2b704683aa210a61c8291
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-10-08 14:36:38 -0600

    Initial Commit

---
+++ TCA Stack-based Navigation/TCA_Stack_based_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Stack_based_NavigationApp.swift
+//  TCA Stack-based Navigation
+//
+//  Created by Adam Garrett-Harris on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Stack_based_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
