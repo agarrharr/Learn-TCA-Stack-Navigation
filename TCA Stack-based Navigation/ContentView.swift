0da97ec6e352941f69861aaae97ec25bf5f91e14
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-10-18 22:53:18 -0600

    Use trailing closure

--- TCA Stack-based Navigation/ContentView.swift
+++ TCA Stack-based Navigation/ContentView.swift
@@ -3,9 +3,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        DevicesView(store: Store(initialState: DevicesFeature.State(devices: []), reducer: {
+        DevicesView(store: Store(initialState: DevicesFeature.State(devices: [])) {
             DevicesFeature()._printChanges()
-        }))
+        })
     }
 }
 
